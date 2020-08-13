857f8cf3a2641bcca2047bd24d03dc35349d9b4b
froster02 <arushnaudiyal@gmail.com>
2020-08-13 19:44:46 +0530

    fast cpp program

--- p1.cpp
+++ p1.cpp
@@ -1,6 +1,21 @@
-#include <iostream> 
+#include <iostream>
 using namespace std;
+#define max 10
+#define endl "\n"
 int main(){
-    cout << "hello world";
+    
+    //to take faster input
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int a[max] = {0};
+
+    //size of an array
+    cout << sizeof(a) << "\n";
+
+    //number of elements in an array
+    int n = sizeof(a) / sizeof(int);
+
+    cout << n;
     return 0;
 }
---
+++ p3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#define maximum 100
+#define endl "\n"
+using namespace std;
+int main(){
+
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, a[maximum], large = INT_MIN, small = INT_MAX;
+    cin >> n;
+    for(int i=0; i<n; i++)
+        cin >> a[i];
+    
+    //algo 1 
+    for(int i=0; i<n; i++){
+        large = max(large, a[i]);
+        small = min(small, a[i]);
+    }
+
+    //algo 2
+    // for(int i=0; i<n; i++){
+    //     if(large < a[i])
+    //         large = a[i];
+    //     if(small > a[i])
+    //         small = a[i];
+    // }
+
+    cout << "Maximum : " << large << "\nMinimum : " << small << endl;
+
+}
