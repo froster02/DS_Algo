c9cb85d3555502d1a7af747117a98128a49c4ed7
froster02 <arushnaudiyal@gmail.com>
2020-08-16 23:39:36 +0530

    update

---
+++ p12.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdio>
+#define max 100
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, a[max], cumSum[max], maxSum = 0, currentSum = 0;
+    cin >> n;
+    cin >> a[0];
+    cumSum[0] = a[0];
+    for(int i=1; i<n; i++) {
+        cin >> a[i];
+        cumSum[i] = cumSum[i-1] + a[i];
+    }
+
+    for(int i=0; i<n; i++){
+        for(int j=i; j<n; j++){
+            currentSum = 0;
+            currentSum = cumSum[j] - cumSum[i-1];
+            if(currentSum > maxSum)
+                maxSum = currentSum;
+        }
+    }
+    cout << maxSum << endl;
+
+    return 0;
+}
