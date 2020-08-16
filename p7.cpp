19fd07571e1fcfd9124eae46fbc088cd68574958
froster02 <arushnaudiyal@gmail.com>
2020-08-16 11:06:02 +0530

    update

--- p7.cpp
+++ p7.cpp
@@ -2,25 +2,27 @@
 #include <cstdio>
 #define max 100
 using namespace std;
-void insertionSort (int a[], int n){
-    for(int i=0; i<n; i++){
+void insertionSort(int a[], int n) {
+    for (int i = 0; i < n; i++) {
         int temp = a[i];
-        int j = i-1;
-        while (j >= 0 && a[j] > temp){
-            a[j+1] = a[j];
+        int j = i - 1;
+        while (j >= 0 && a[j] > temp) {
+            a[j + 1] = a[j];
             j--;
         }
-        a[j+1] = temp;
+        a[j + 1] = temp;
     }
 }
-int main(){
-    int n, a[max] = {0};
+int main() {
+    int n, a[max] = {
+        0
+    };
     cin >> n;
-    for(int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
         cin >> a[i];
 
     insertionSort(a, n);
 
-    for(int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
         cout << a[i] << "|";
 }
