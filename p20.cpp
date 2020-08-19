07424407a5ee0055de8cb90709b4a48c6b3655f9
froster02 <arushnaudiyal@gmail.com>
2020-08-19 20:44:26 +0530

    update

--- p20.cpp
+++ p20.cpp
@@ -1,17 +1,45 @@
 #include <iostream>
+#include <cstdio>
+#include <algorithm>
 using namespace std;
 #define max 100
 #define endl "\n"
 
-void rotateImage(int a[][max], int n){
-    `
-    cout << "*******************";
-    for(int j=n; j>=1; j--){
-        for(int i=1; i<=n; i++)
+//Method 1
+void rotateImage(int a[][max], int n) {
+
+    cout << "*******************" << endl;
+    for (int j = n; j >= 1; j--) {
+        for (int i = 1; i <= n; i++)
             cout << a[i][j] << " ";
         cout << endl;
     }
 }
+
+// //Method 2
+// void rotateImage(int a[][max], int n) {
+//     cout << "******************" << endl;
+//     for (int j = n; j >= 1; j--) {
+//         for (int i = 1; i <= n; i++) {
+//             swap(a[i][j], a[j][i]);
+//         }
+//         cout << endl;
+//     }
+// }
+
+// //Method 3
+// void rotateImage(int a[][max], int n) {
+//     cout << "*********************" << endl;
+//     for (int i = 0; i < n; i++)
+//         reverse(a[i], a[i] + n);
+//     for (int j = n; j >= 1; j--) {
+//         for (int i = 1; i <= n; i++) {
+//             swap(a[i][j], a[j][i]);
+//         }
+//         cout << endl;
+//     }
+
+// }
 
 int main() {
     ios_base::sync_with_stdio(false);
