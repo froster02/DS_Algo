1d7cf46d3519aa48fff282b7f7923e3477f8ff60
froster02 <arushnaudiyal@gmail.com>
2020-08-19 20:01:58 +0530

    update

---
+++ p19.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cstdio>
+#define max 100
+#define endl "\n"
+using namespace std;
+//************************************************************   ERROR      *********************************************
+void magicalPark(int a[][max], int n, int m, int s, int k) {
+    bool success = true;
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            char ch = a[i][j];
+            if (s < k) {
+                success = false;
+                break;
+            }
+			if (ch == '*')
+                s += 5;
+            else if (ch == '.')
+                s -= 2;
+            else{
+                break;
+			}
+
+			if(j != n-1)
+            	s--;
+        }
+    }
+    if (success) {
+        cout << "YES" << endl;
+        cout << s << endl;
+    }
+    else 
+        cout << "NO" << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, m, a[max][max], s, k;
+    cin >> n >> m >> k >> s;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    magicalPark(a, n, m, s, k);
+
+    return 0;
+}
---
+++ p20.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+#define max 100
+#define endl "\n"
+
+void rotateImage(int a[][max], int n){
+
+    cout << "*******************";
+    for(int j=n; j>=1; j--){
+        for(int i=1; i<=n; i++)
+            cout << a[i][j] << " ";
+        cout << endl;
+    }
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, a[max][max];
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    rotateImage(a, n);
+
+    return 0;
+}
