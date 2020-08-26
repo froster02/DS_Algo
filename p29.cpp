afa066bbe64fe7bbdc213920d3a110e08fa69ac4
froster02 <arushnaudiyal@gmail.com>
2020-08-26 09:34:15 +0530

    update

--- p29.cpp
+++ p29.cpp
@@ -1,7 +1,7 @@
 //******************************************************||         ERROR        ||************************************************
 
 //sum of all submatrices of a given matrix (O^6)
-#include <iostream>
+#include <iostream>s
 #include <cstdio>
 #define max 100
 using namespace std;
@@ -9,29 +9,28 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int n, a[max][max], sum = 0;
+    int a[max][max];
+    int n;
     cin >> n;
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             cin >> a[i][j];
         }
     }
-
-    for(int li=0; li < n-1; li++){
-        for(int lj=0; lj < n-1; lj++){
-            for(int bi = li; bi < n-1; bi++){
-                for(int bj = lj; bj < n-1; bj++){
-                    for(int i = li; i < bi; i++){
-                        for(int j = lj; j < bj; j++){
-                            sum += a[i][j];
+    for (int li = 0; li < n; li++) {
+        for (int lj = 0; lj < n; lj++) {
+            for (int bi = li + 1; bi < n; bi++) {
+                for (int bj = lj + 1; bj < n; bj++) {
+                    for (int i = li; i <= bi; i++) {
+                        for (int j = lj; j <= bj; j++) {
+                            sum = sum + a[i][j];
                         }
                     }
                 }
             }
         }
     }
-
-    
 
     cout << "Sum : " << sum << endl;
 
