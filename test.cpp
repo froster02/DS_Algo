47072c496bf1c8a9072b60a315102486de51fbe9
froster02 <arushnaudiyal@gmail.com>
2020-08-27 10:00:03 +0530

    update

--- test.cpp
+++ test.cpp
@@ -2,38 +2,16 @@
 #include <algorithm>
 using namespace std;
 
-// void bubbleSort(int a[], int n){
-
-//     for(int i=0; i < n - 1; i++){
-//         for(int j = 0; j < n-1-i; j++){
-//             if(a[j] > a[j + 1])
-//                 swap(a[j], a[j + 1]);
-//         }
-//     }
-// }
-
-// void insertionSort(int a[], int n){
-
-//     for(int i = 1; i < n; i++){
-//         int key = a[i];
-//         int j = i - 1;
-//         while (j >= 0 && key < a[j]){
-//             a[j + 1] = a[j];
-//             j--;
-//         }
-//         a[j + 1] = key;
-//     }
-// }
-
-void selectionSort(int a[], int n){
-    int mid = 0;
-    for(int i = 0; i < n - 1; i++){
-        mid = i;
-        for(int j = i + 1; j < n; j++){
-            if(a[j] < a[mid])
-                mid = j;
+void insertionSort(int a[], int n){
+    int key = 0, j = 0;
+    for(int i = 1; i < n; i++){
+        key = a[i];
+        j = i - 1;
+        while (j >= 0 && key < a[j]){
+            a[j + 1] = a[j];
+            j--;
         }
-        swap(a[i], a[mid]);
+        a[j + 1] = key;
     }
 }
 
@@ -45,8 +23,8 @@ int main(){
         cin >> a[i];
 
     //bubbleSort(a, n);
-    //insertionSort(a, n);
-    selectionSort(a, n);
+    insertionSort(a, n);
+    //selectionSort(a, n);
 
     for(int i=0; i<n; i++)
         cout << a[i] << "|";
