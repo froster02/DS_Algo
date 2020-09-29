ce97d8bd993a4ce360218bd1a25be2ddd3579ae3
froster02 <arushnaudiyal@gmail.com>
2020-09-29 21:53:55 +0530

    update

--- p41.cpp
+++ p41.cpp
@@ -8,12 +8,10 @@ using namespace std;
 //maximum time taken by lowest ranked cook
 int max_cooking_time(int pratha, int slow_cook) {
     int count = 0, max_time = 0;
-    //while (count < pratha) {
     for(int i=1; count < pratha; i++){
         max_time = max_time + (i * slow_cook);
         count++;
     }
-    //cout << max_time << endl;
     return max_time;
 }
 
@@ -26,27 +24,21 @@ bool isPossible(int a[], int pratha, int cooks, int mid) {
         int j = 1;
         while (1) {
             time = time + (j * a[i]);
-            //cout << time << endl;
             j++;
             if (time > mid)
                 break;
             count++;
         }
     }
-    //cout << count << endl;
     return (count >= pratha);
 }
 
 //calculate best time taken by cooks
 void cookPratha(int a[], int pratha, int cooks, int slow_cook) {
     int ans = -1, s = 0, e = max_cooking_time(pratha, slow_cook);
-    //while (s < e) {
-    //cout << e << endl;
     while(s <= e) {
         int mid = (s + e) / 2;
-        //cout << mid << endl;
         if (isPossible(a, pratha, cooks, mid)) {
-            //cout << mid << endl;
             ans = mid;
             e = mid - 1;
         } else {
@@ -69,9 +61,7 @@ int main() {
         for (int j = 0; j < cook; j++) {
             cin >> a[j];
             slow_cook = max(slow_cook, a[j]);
-            //cout << slow_cook << endl;
         }
-        //cout << "c " << cook << endl;
         cookPratha(a, pratha, cook, slow_cook);
     }
     return 0;
