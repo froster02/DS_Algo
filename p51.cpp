be7883d5a84185b6eb3c84d26c83bb898301366c
froster02 <arushnaudiyal@gmail.com>
2020-10-01 22:06:14 +0530

    update

---
+++ p51.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#define IO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define endl '\n'
+using namespace std;
+
+int main(){
+    IO;
+
+    
+
+    return 0;
+}
