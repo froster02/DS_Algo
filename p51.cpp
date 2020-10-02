3965009fc2355fde36c1cd25320a502fd49eec8f
froster02 <arushnaudiyal@gmail.com>
2020-10-02 08:39:41 +0530

    update

--- p51.cpp
+++ p51.cpp
@@ -1,8 +1,7 @@
+/************************************************************   |ERROR|    *******************************************************/
 #include <iostream>
 #include <cstdio>
-#include <vector>
-#include <algorithm>
-#include <iterator>
+#include "vector51.h"
 #define IO ios_base::sync_with_stdio(false); cin.tie(NULL);
 #define endl '\n'
 using namespace std;
@@ -10,7 +9,17 @@ using namespace std;
 int main(){
     IO;
 
-    
+        vector v;
+        v.push_back(1);
+        v.push_back(2);
+        v.push_back(3);
+        v.pop_back();
+        v.push_back(4);
+        
+        for(int i=0; i<v.size(); i++)
+            cout << v[i] << "|";
+        cout << endl;
+        //cout << "Capacity : " << Capacity() << endl;
 
     return 0;
 }
--- vector51.h
+++ vector51.h
@@ -1,3 +1,4 @@
+/************************************************************   |ERROR|    *******************************************************/
 class Vector51 {
     int cs;
     int ms;
@@ -11,9 +12,10 @@ public:
     }
     void push_back(int d){
         if(cs == ms){
+            //array is fulls
             int *oldArr = arr;
-            arr = new int[2*ts];
-            ts = 2 * ts;
+            arr = new int[2*ms];
+            int ms = 2 * ms;
             for(int i=0; i<cs; i++)
                 arr[i] = oldArr[i];
             //clear the old memeory
@@ -21,5 +23,23 @@ public:
         }
         arr[cs] = d;
         cs++;
+    }
+    void pop_back(){
+        cs--;
+    }
+    int front(){
+        return arr[0];
+    }
+    int back(){
+        return arr[cs - 1];
+    }
+    bool empty() {
+        return cs == 0;
+    }
+    int Capacity(){
+        return ms;
+    }
+    int at( int i){
+        return arr[i];
     }
 };
