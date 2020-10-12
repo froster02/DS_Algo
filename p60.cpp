d71094436c5d77691254f9d29668d62567238901
froster02 <arushnaudiyal@gmail.com>
2020-10-12 06:09:45 +0530

    update

--- p60.cpp
+++ p60.cpp
@@ -1,32 +1,9 @@
-/* BITWISE -> 
-We are given an array containg n numbers. All the numbers are present twice except for two numbers which are present only once.
-Find the unique numbers in linear time without using any extra space. ( Hint - Use Bitwise )
-
-Input Format
-First line contains the number n. Second line contains n space separated number.
-
-Constraints
-n < 10^5
-
-Output Format
-Output a single line containing the unique numbers separated by space
-
-Sample Input
-4
-3 1 2 1
-Sample Output
-2 3
-Explanation
-Smaller number comes before larger number */
-
 #include <iostream>
-#include <cstdio>
-#define IO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#include <climits>
 #define endl '\n'
 using namespace std;
 
 int main() {
-    IO;
     
     //input
     int n, no, a[100005];
@@ -44,7 +21,7 @@ int main() {
 
     //position at which set bit is located
     int pos = 0;
-    while ((temp & 1) != 1) {
+    for(;((temp & 1) != 1) ;)  {
         pos++;
         temp = temp >> 1;
     }
@@ -62,6 +39,8 @@ int main() {
 
     //XOR with the previous result of unique elements and the number where it found the set bit
     y = res ^ x;
+    cout << "res : " <<res << endl;
+    cout << "x : " << x << endl;
 
     //output the value of unique numbers
     cout << min(x, y) << " " << max(x, y) << endl;
