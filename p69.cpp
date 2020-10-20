7ced8b8fbc8f81606d86f1dc45617505f0ae860e
froster02 <arushnaudiyal@gmail.com>
2020-10-20 07:22:12 +0530

    update

---
+++ p69.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void prime_factor(int n){
+    vector < pair<int, int> > v;
+    int cnt;
+    for(int i = 2; i * i <= n; i++){
+        //keep on dividing till it is divisible
+        if(n % i == 0){
+            cnt = 0;
+            while(n % i == 0){
+                cnt++;
+                n = n / i;
+            }
+            v.push_back(make_pair(i, cnt));
+        }
+    }
+    if(n != 1){
+        //whatever is left is a prime number in itself
+        v.push_back(make_pair(n, 1));
+    }
+    //print factors 
+    for(auto p : v)
+        cout << p.first << "^" << p.second << endl;
+    
+}
+
+int main(){
+
+    int n;
+    cin >> n;
+    prime_factor(n);
+
+    return 0;
+}
