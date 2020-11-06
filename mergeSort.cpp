057c656c08abb099052a5fd492172881fa6baafe
froster02 <arushnaudiyal@gmail.com>
2020-11-06 18:27:28 +0530

    update

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -1,8 +1,61 @@
-#include <iosteam>
+#include <iostream>
+#define maxm 100
 using namespace std;
-int main(){
 
-    
+void merge(int *a, int s, int e) {
+
+    int mid = (s + e) / 2;
+    int i = s;
+    int j = mid + 1;
+    int k = s;
+
+    int temp[maxm];
+
+    while ((i <= mid) && (j <= e)) {
+        if (a[i] < a[j])
+            temp[k++] = a[i++];
+        else
+            temp[k++] = a[j++];
+    }
+
+    while (i <= mid) {
+        temp[k++] = a[i++];
+    }
+    while (j <= e) {
+        temp[k++] = a[j++];
+    }
+
+    for (int i = s; i <= e; i++)
+        a[i] = temp[i];
+
+}
+
+void mergeSort(int a[], int s, int e) {
+    //base case 1 or 0 element 
+    if (s <= e)
+        return;
+    //follow step : Divide, Sort, Merge
+    int mid = (s + e) / 2;
+
+    mergeSort(a, s, mid);
+    mergeSort(a, mid + 1, e);
+
+    //merge the two above parts
+    merge(a, s, e);
+}
+
+int main() {
+
+    int n, a[maxm];
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    mergeSort(a, 0, n - 1);
+
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " ";
 
     return 0;
 }
