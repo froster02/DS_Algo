639b3b0e516c751593a1b9686c8a46c69df84e96
froster02 <arushnaudiyal@gmail.com>
2020-11-08 10:34:20 +0530

    update

---
+++ digit_to_word.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+using namespace std;
+
+char words[][10] = {
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine"
+};
+
+void printSpelling(int n) {
+    //base case
+    if (n == 0)
+        return;
+
+    //recursive case to first print (204)
+    printSpelling(n / 10);
+
+    int digit = n % 10;
+
+    cout << words[digit] << " ";
+    return;
+}
+
+// void printSpelling_reverse(int n){
+//     //base case 
+//     if(n == 0)
+//         return;
+//     //recursive case to first print (048)
+//     printSpelling_reverse(n / 10);
+
+//     int digit = n % 10;
+
+//     cout << words[digit] << " ";
+//     return;
+// }
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    printSpelling(n);
+
+    //printSpelling_reverse(n);
+
+    cout << endl;
+
+    return 0;
+}
