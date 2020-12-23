53ea0384a994ea0612e60aca7c1bb09f23b03cb9
Your Name <you@example.com>
2020-12-23 08:17:36 +0530

    update

---
+++ knapsackClassical.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+void profit(int n, int c, int *wt, int *pr){
+    
+    //base case
+    if(n==0 || c==0)
+        return 0;
+    
+    int ans, exclude, include;
+    ans = exclude = include = 0;
+
+    //include
+    if(wt[n-1] <= c){
+        include = pr[n-1] + profit(n-1, c - wt[n-1], wt, pr);
+    }
+
+    //exclude
+    exclude = profit(n-1, c, wt, pr);
+
+    ans = max(include, exclude);
+
+    return ans;
+}
+
+int main(){
+
+    int weight[] = {1, 2, 3, 5};
+    int price[] = {40, 20, 30, 100};
+
+    int n = 4, c = 7;
+
+    cout << profit(n, c, weight, price);
+
+    return 0;
+}
