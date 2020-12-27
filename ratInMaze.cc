a4c21a727097d5182f96a8aba34aa133a36c5a60
Your Name <you@example.com>
2020-12-27 18:08:55 +0530

    update

--- ratInMaze.cc
+++ ratInMaze.cc
@@ -33,15 +33,14 @@ bool ratInMaze(char maze[10][10], int solution[][10], int i, int j, int m, int n
     solution[i][j] = 1;
 
     //recursive calls for checking weather mice can go Right or Down
-    bool RightSide = ratInMaze(maze, solution, i, j + 1, m, n, count); // i resemble the right side 
-    bool DownSide = ratInMaze(maze, solution, i + 1, j, m, n, count); // j resemble the down side 
+    bool RightSide = ratInMaze(maze, solution, i, j + 1, m, n); // i resemble the right side 
+    bool DownSide = ratInMaze(maze, solution, i + 1, j, m, n); // j resemble the down side 
 
     //backtracking (fill the visited case with 0 while comes backward to the same path)
     solution[i][j] = 0;
 
-    //weathe rightside or downside is possible consider it as true for visiting
+    //wether rightside or downside is possible consider it as true for visiting
     if (RightSide || DownSide) {
-        count++;
         return true;
     }
 
