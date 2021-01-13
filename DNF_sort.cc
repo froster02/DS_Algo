4873f4f2237e93f72d34cda732c4787bb0a8b774
Your Name <you@example.com>
2021-01-13 19:10:19 +0530

    update

---
+++ DNF_sort.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+void dnf_sort(int a[], int n){
+    //very useful for sorting 0s, 1s, and 2s
+    int low = 0;
+    int high = n - 1;
+    int mid = 0;
+
+    while (mid <= high) {
+
+        //make the starting area for 0s
+        if(a[mid] == 0){
+            swap(a[mid], a[low]);
+            low++;
+            mid++;
+        }
+        //make the middle area for 1s
+        if(a[mid] == 1){
+            mid++;
+        }
+        //make the ending area for 2s
+        if(a[mid] == 2){
+            swap(a[mid], a[high]);
+            high--;
+        }
+    }
+}
+
+int main(){
+
+    int a[] = {2,1,1,1,1,0,1,1,1,0,0,2,0};
+    int n = sizeof(a) / sizeof(int);
+
+    dnf_sort(a, n);
+
+    for(int i : a)
+        cout << i << "|";
+    
+    cout << endl;
+
+    return 0;
+}
