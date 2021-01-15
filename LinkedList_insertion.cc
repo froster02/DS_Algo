217d8da22a0a7687beef428a59d94a708f0e32f8
Your Name <you@example.com>
2021-01-15 08:23:25 +0530

    update

---
+++ LinkedList_insertion.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+class node {
+    public:
+    int data;
+    node *next;
+
+    node(int d){
+        data = d;
+        next = NULL;
+    }
+};
+
+/*  passing a pointer variable by Reference so that head value refelect in main also
+    since by doing this *&head we have access of the actual head all the time */
+void insertAtHead(node *&head, int d){
+    if(head == NULL) {
+        head = new node(d);
+        return;
+    }
+
+    node *newnode = new node(d);
+    newnode->next = head;
+    head = newnode;
+
+}
+
+void print(node *head){
+    while(head != NULL){
+        cout << head->data<<"->";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main(){
+    time_t s = clock();
+
+    node *head = NULL;
+    insertAtHead(head, 3);
+    insertAtHead(head, 2);
+    insertAtHead(head, 1);
+    insertAtHead(head, 0);
+
+    print(head);
+    print(head);
+
+    time_t e = clock();
+    cout << "\ntime taken : " << e - s << " ms" << endl;
+    return 0;
+}
--- tempCodeRunnerFile.cc
+++ tempCodeRunnerFile.cc
@@ -1,25 +1,6 @@
-#include <iostream>
-using namespace std;
-
-void wave_sort(int a[], int n) {
-    for (int i = 0; i < n; i+=2) {
-        if ((a[i - 1] > a[i])) {
-            swap(a[i], a[i - 1]);
-        
-        if (a[i] < a[i + 1])
-            swap(a[i], a[i + 1]);
-    }
-}
-
-int main() {
-
-    int n, a[100];
-    cin >> n;
-    for (int i = 0; i < n; i++)
-        cin >> a[i];
-
-    wave_sort(a, n);
-    for (int i = 0; i < n; i++)
-        cout << a[i] << "|";
-    cout << endl;
-}
+node *newnode(int d) {
+//     node *temp = new node();
+//     temp -> data = d;
+//     temp -> next = NULL;
+//     return temp;
+// }
