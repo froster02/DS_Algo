f3a4675eaa8bfd2dd7cd34217c46edcc97a9c4ca
Your Name <you@example.com>
2021-01-16 20:07:10 +0530

    update

---
+++ FloydCycle.cc
@@ -0,0 +1,80 @@
+#include <iostream>
+using namespace std;
+
+struct node {
+    int data;
+    node *next;
+
+    node(int d) {
+        data = d;
+        next = NULL;
+    };
+};
+
+void push(int data){
+    if(head == NULL) {
+        head = new node(data);
+        return;
+    }
+
+    node *temp = new node(data);
+    temp->next = head;
+    head = temp;
+    return;
+}
+
+void print(node *head){
+    while(head != NULL){
+        cout << head->data << "->";
+        head = head->next;
+    }
+    return;
+}
+
+void removeLoop(node slow, node *head) {
+    node *p1 = head;
+    node *p2 = slow;
+
+    while(p2->next != slow || p2->next == head){
+        p1 = p1->next;
+        p2 = p2->next;
+        if(p2->next == p1)
+            break;
+    }
+    p2->next = NULL;
+}
+
+bool deleteCycle(node *&data){
+    node *fast = head;
+    node *slow = head;
+    
+    while (fast != NULL && fast->next != NULL) {
+        fast = (fast->next)->next;
+        slow = slow->next;
+
+        if(fast == slow){
+            removeLoop(slow, head);
+            return true;
+        }
+    }
+    return false;
+}
+
+int main(){
+
+    cout << endl;
+    node *head = push(50);
+    head->next = push(20);
+    head->next->next = push(15);
+    head->next->next->next = push(4);
+    head->next->next->next->next = push(10);
+
+    head->next->next->next->next->next = head->next->next;
+    
+    print(head);
+
+    deleteCycle(head);
+
+    cout << endl <<endl;
+    return 0;
+}
--- tempCodeRunnerFile.cc
+++ tempCodeRunnerFile.cc
@@ -1,6 +1,41 @@
-node *newnode(int d) {
-//     node *temp = new node();
-//     temp -> data = d;
-//     temp -> next = NULL;
-//     return temp;
+//node* merge(node *A, node *B){
+//     //base case
+//     if(A == NULL)
+//         return B;
+//     if(B == NULL)
+//         return A;
+    
+//     //take a head pointer
+//     node *C;
+
+//     if((A->data) < (B->data)) {
+//         C = A;
+//         C->next = merge(A->next, B);
+//     } else {
+//         C = B;
+//         C->next = merge(A, B->next);
+//     }
+//     return C;
+// }
+
+// node* mergesort(node *head) {
+//     //base case
+//     if(head == NULL || head->next == NULL)
+//         return head;
+    
+//     //rec case
+//     //1. Break
+//     node *mid = middleElement(head);
+//     node *a = head;
+//     node *b = mid->next;
+
+//     mid->next = NULL;
+
+//     //2. Rec sort the two part
+//     a = mergesort(a);
+//     b = mergesort(b);
+
+//     //3. Merge 
+//     node *c = merge(a, b);
+//     return c;
 // }
