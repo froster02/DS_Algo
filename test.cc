446342b6d4c1d7d5aa23c3254143ff023482e00a
Your Name <you@example.com>
2021-01-17 15:16:47 +0530

    update

--- test.cc
+++ test.cc
@@ -1,108 +1,43 @@
-#include <iostream>
+#include <stdio.h> 
 
-using namespace std;
-struct node {
-    int data;
-    node *next;
-};
-
-node *newnode(int key) {
-    node * temp = new node();
-    temp -> data = key;
-    temp -> next = NULL;
-    return temp;
-}
-
-void IAB(node **head, int key) {
-    node * temp = newnode(key);
-    temp -> next = *head;
-    * head = temp;
-}
-void IAE(node **head, int key) {
-    node *temp = newnode(key);
-    if ( *head == NULL) {
-        node *itr = * head;
-        while (itr -> next != NULL)
-            itr = itr -> next;
-    } else {
-        *head = temp;
-    }
-}
-void IBK(node **head, int key, int k) {
-    if(*head == NULL || (*head)->data == k){
-        IAF(*head, key);
-        return;
-    }
-    node *itr = *head, *temp = newnode(key);
-    while(itr->next != NULL && itr->next->data != k)    itr = itr->next;
-    temp->next = itr->next;
-    itr->next = temp;
-}
-
-void IAK(node **head, int key, int k) {
-    node *itr = *head, *temp = newnode(key);
-    while(itr->next != NULL && itr->next->data != k){
-        itr = itr->next;
-    temp->next = itr->next;
-    itr->next = temp;
-}
-
-bool search(node *head, int key) {
-    while (head != NULL) {
-        if (head -> data == key)
-            return true;
-        head = head -> next;
-    }
-    return false;
-}
-
-void Print(node * head) {
-    while (head != NULL) {
-        cout << head -> data << "->";
-        head = head -> next;
-    }
-
-}
-int main() {
-    node * head = null;
-    int ch;
-    int key;
-    do {
-
-        cout << "Menu\n1.Iab\n3.IAE\n3.IBK\n4.IAK\n5.search\n6.print\nEnter";
-        cin >> ch;
-        switch (ch) {
-            case 1:
-                cin >> key;
-                IAB( & head, key, k);
-                break;
-            case 2:
-                cin >> key;
-                IAE( & head, key);
-                break;
-            case 3:
-                cin >> key;
-                IAK( & head, key, k);
-
-                break;
-            case 4: cin >> key >> k;
-                    IAK(&head, key, k);
-                break;
-            case 5:
-                cin >> key;
-                if (search(head, key)) {
-                    cout << key << " found in LL\n";
-                } else {
-                    cout << key << " not found\n";
-                }
-                break;
-            case 6:
-                print("Head");
-                break;
-
-            default:
-                cout << "Exiting Program BYE";
+int find1(int i, int j, int a[], int key, int n) {
+    int l = i + 1, h = j - 1, mid, flag = 0;
+    mid = l + (h - l) / 2;
+    while (l <= h) {
+        if (a[mid] == key) 
+            return mid;
+        if (a[mid] < key) {
+            l = mid + 1;
+            mid = l + (h - l) / 2;
+        } else {
+            h = mid - 1;
+            mid = l + (h - l) / 2;
         }
     }
+    return -1;
 }
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    while (t--) {
+        int i, j, k, n, p;
+        scanf("%d", &n);
+        int a[n];
+        for (i = 0; i < n; i++)
+            scanf("%d", &a[i]);
+        for (i = n - 1; i > 0; i--) {
+            for (j = 0; j < n; j++) {
+                p = find1(j, i, a, a[i] - a[j], n);
+                if (p != -1) {
+                    printf("%d %d %d", (j+1), (p+1), (i+1));
+                    printf("\n");
+                    break;
+                }
+            }
+            if (p != -1) break;
+        }
+        if (p == -1)
+            printf("No Sequence Found");
+    }
 }
