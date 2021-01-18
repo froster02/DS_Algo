12a2bc097bc2752108110ba0be483d7851a26205
Your Name <you@example.com>
2021-01-18 18:09:36 +0530

    update

---
+++ Stack_stockSpan.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+
+#include <stack>
+
+using namespace std;
+
+void stockSpan(int prices[], int n, int span[]) {
+    stack < int > s;
+    s.push(0);
+
+    for (int i = 0; i < n - 1; i++) {
+        int currentPrice = prices[i];
+        while ((!s.empty()) && (prices[s.top()] <= currentPrice)) {
+            s.pop();
+        }
+        if (!s.empty()) {
+            int prev_highest = s.top();
+            span[i] = i - prev_highest;
+        } else {
+            span[i] = i + 1;
+        }
+        s.push(i);
+    }
+}
+
+int main() {
+    cout << endl;
+    int prices[] = {
+        100,
+        80,
+        60,
+        70,
+        60,
+        75,
+        85
+    };
+    int n = sizeof(prices) / sizeof(int);
+    int span[100000] = {
+        0
+    };
+
+    stockSpan(prices, n, span);
+    for (int i = 0; i < n; i++) {
+        cout << span[i] << ", ";
+    }
+    cout << endl << endl;
+    return 0;
+}
