1c6b70a3addde96f6bea87291cce84ce0a433f98
Your Name <you@example.com>
2021-01-28 11:28:10 +0530

    update

---
+++ Set.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main(){
+
+    int a[] = {1,2,3,5,4,6,6,7,8};
+    int n = sizeof(a)/sizeof(int);
+
+    set<int> s;
+    for(int i = 0; i < n; i++)
+        s.insert(a[i]);
+
+    s.erase(7);
+
+    for(set<int> :: iterator it = s.begin(); it != s.end(); it++)
+        cout << *it << ", ";
+
+    return 0;
+}
---
+++ Set_makePair.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main(){
+
+    set<pair<int, int> > s;
+    s.insert(make_pair(10,1));    
+    s.insert(make_pair(20,1));    
+    s.insert(make_pair(20,100));    
+    s.insert(make_pair(5,1));    
+    s.insert(make_pair(5,3));    
+    s.insert(make_pair(5,2));    
+
+    for(auto p : s)
+        cout << p.first << " , " << p.second << endl;
+    
+    cout << endl;
+
+    s.erase(make_pair(5,3));
+    s.insert(make_pair(5,4));
+
+    s.erase(s.find(make_pair(20, 100)));
+    s.insert(make_pair(5,4));
+
+    for(auto p : s)
+        cout << p.first << " & " << p.second << endl; 
+
+    return 0;
+}
