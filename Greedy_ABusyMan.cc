046e6540505e2ab87a69eaec43aa88e3fb875a5e
froster02 <arushnaudiyal@gmail.com>
2021-02-09 22:24:20 +0530

    update

---
+++ Greedy_ABusyMan.cc
@@ -0,0 +1,46 @@
+//https://www.spoj.com/problems/BUSYMAN/
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+//compare according to finishing time
+bool compare(pair<int, int> p1, pair<int, int> p2){
+    return p1.second < p2.second;
+}
+
+int main(){
+
+    int t, n, s, e;
+    cin >> t;
+
+    vector<pair<int, int> > v(0);
+
+    while(t--){
+        cin >> n;
+        for(int i = 0; i < n; i++){
+            cin >> s >> e;
+            v.push_back(make_pair(s, e));
+        }
+        //activity selection (pick activity which finishes early)
+        sort(v.begin(), v.end(), compare);
+
+        //start picking activity
+        int res = 1;
+        int finish = v[0].second;
+
+        //iterate over remaing activity
+        for(int i = 1; i < n; i++){
+            if(v[i].first >= finish){
+                finish = v[i].second;
+                res++;
+            }
+        }
+        cout << res << endl;
+        v.clear();
+
+        return 0;
+    }
+
+    return 0;
+}
