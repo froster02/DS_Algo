2c65df5b6c4265985271d9986df9ac607d60a82f
froster02 <arushnaudiyal@gmail.com>
2021-02-10 13:20:04 +0530

    update

---
+++ Greedy_biasedStanding.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    int a[100] = {0};
+    int t;
+    cin >> t;
+    while (t--) {
+        memset(a, 0, sizeof(a));
+        string name;
+        int n, r;
+        cin >> n;
+
+        for (int i = 0; i < n; i++) {
+            cin >> name >> r;
+            a[r]++;
+        }
+
+        //greedy approach = assign the team nearest rank available
+        int actural_rank = 1, sum = 0;
+        for(int i = 1; i <= n; i++){
+            while(a[i]){
+                sum = sum + abs(actural_rank - i);
+                a[i]--;
+                actural_rank++;
+            }
+        }
+        cout << sum << endl;
+    }
+}
---
+++ Greedy_loadBalance.cc
@@ -0,0 +1,37 @@
+//https://www.spoj.com/problems/BALIFE/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a[1000];
+    int n, i, val, diff;
+
+    while(1){
+        int max_load = 0, load = 0;
+        cin >> n;
+
+        if(n == -1)
+            break;
+
+        for(int i = 0; i < n; i++) {
+            cin >> a[i];
+            load = load + a[i];
+        }
+        if(load % n != 0){
+            cout << "-1" << endl
+            continue;
+        }
+        //find the load that is to be divided equally
+        load = load / n;
+
+        //greedy step
+        for(int i = 0; i < n; i++){
+            
+        }
+        cout << max_load;
+    }
+
+    return 0;
+}
