89f87fd7dd22467c368ad5b59a8d80c901e5db12
froster02 <arushnaudiyal@gmail.com>
2021-02-12 18:34:48 +0530

    update

---
+++ DP_moneyExchange.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <climits>
+#include <algorithm>
+#define maxm 100
+using namespace std;
+
+int minCoins(int n, int coins[], int t, int dp[]){
+    //base case
+    if(n == 0)
+        return 0;
+
+    //look up
+    if(dp[n] != 0)
+        return dp[n];
+    
+    //recursive case
+    int ans = INT_MAX;
+    for(int i = 0; i < t; i ++){
+        if(n - coins[i] >= 0) {
+            int subprblm = minCoins(n - coins[i], coins, t, dp);
+            ans = min(ans, subprblm + 1);
+        }
+    }
+    dp[n] = ans;
+    return dp[n];
+}
+
+int main() {
+
+    int n = 15;
+    int coins[] = {1, 7, 10};
+    int dp[maxm] = {0};
+    int t = sizeof(coins) / sizeof(int);
+
+    cout << minCoins(n, coins, t, dp) << endl;
+
+    return 0;
+}
