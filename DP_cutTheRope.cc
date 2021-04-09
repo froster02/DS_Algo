4ca9ffbac66d32ca2a1a14e4f8c14eb09bf51a7f
froster02 <arushnaudiyal@gmail.com>
2021-04-09 18:49:50 +0530

    update

--- DP_cutTheRope.cc
+++ DP_cutTheRope.cc
@@ -17,8 +17,27 @@ int max_profit(int price[], int n){
     return ans;
 }
 
+int max_profit_DP(int price[], int n){
+    
+    int dp[n + 1];
+    dp[0] = 0;
+
+    for(int len = 1; len <= n; len++){
+        int ans = INT_MIN;
+        for(int i = 0; i < len; i++){
+            int cut = i + 1;
+            int current_ans = price[i] + dp[len - cut];
+            ans = max(current_ans, ans);
+        }
+        dp[len] = ans;
+    }
+    return dp[n];
+}
+
 int main(){
     int p[] = {1,5,8,9,10,17,17,20};
     int n = sizeof(p)/sizeof(p[0]);
     cout << max_profit(p, n);
+    cout << endl;
+    cout << max_profit_DP(p, n);
 }
