7e75000a132e93cba2163ba8f0b5db7e338e57b6
froster02 <arushnaudiyal@gmail.com>
2021-04-21 16:06:46 +0530

    update

---
+++ Graph_snakeLadderBFS.cc
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <map>
+#include <climits>
+#include <list>
+#include <queue>
+using namespace std;
+
+template<typename T>
+
+class Graph {
+
+    map<T, list<T> > l;
+
+    public:
+    void addEdge(int x, int y){
+        l[x].push_back(y);
+        l[y].push_back(x);
+    }
+
+    int bfs(T src, T dest){
+    map<T, int> dist;
+    map<T, T> parent;
+    queue<T> q;
+
+    for(auto node_pair : l){
+        T node = node_pair.first;
+        dist[node] = INT_MAX;
+    }
+    q.push(src);
+    dist[src] = 0;
+    parent[src] = src;
+
+    while (!q.empty()){
+        T node = q.front();
+        q.pop();
+
+        for(int nbr : l[node]){
+            if(dist[nbr] == INT_MAX){
+                q.push(nbr);
+                dist[nbr] = dist[node] + 1;
+                parent[nbr] = node;
+            }
+        }
+    }
+    T temp = dest;
+    while(temp != src){
+        cout << temp << "<--";
+        temp = parent[temp];
+    }
+    
+    // cout << endl << "Steps to Reach Dest : " << src << endl;
+    cout << endl << endl;
+
+    return dist[dest];
+}
+};
+
+int main(){
+    int board[50] = {0};
+
+    board[2] = 13;
+    board[5] = 2;
+    board[9] = 18;
+    board[18] = 11;
+    board[17] = -13;
+    board[20] = -14;
+    board[24] = -8;
+    board[25] = 10;
+    board[32] = -2;
+    board[34] = -22;
+
+    Graph<int> g;
+    for(int i = 0; i <= 36; i++){
+        for(int dice = 1; dice <= 6; dice++){
+            int j = i + dice;
+            j = j + board[j];
+            if(j <= 36){
+                g.addEdge(i, j);
+            }
+        }
+    }
+
+    g.addEdge(36, 36);
+
+    cout << "Path : " << g.bfs(0, 36);
+
+    return 0;
+}
