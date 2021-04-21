921056aecd546f87b152512d7292a4d919eda79b
froster02 <arushnaudiyal@gmail.com>
2021-04-21 21:15:37 +0530

    update

---
+++ Graph_TopologicalSortingBFS.cc
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <map>
+#include <list>
+#include <queue>
+#include <cstring>
+using namespace std;
+
+class Graph{
+    list<int> *l;
+    int V;
+
+public:
+    Graph(int V){
+        this->V = V;
+        l = new list<int>[V];
+    }
+
+    void addEdge(int x, int y){
+        //directed graph
+        l[x].push_back(y);
+    }
+
+    void topological_sort(){
+        //in-degree
+        int *indegree = new int[V];
+        //0 in-degree
+        for(int i = 0; i < V; i++)
+            indegree[i] = 0;
+
+        //update in-degree by traversing edges x->y
+        //in-degree[y]++;
+        for(int i = 0; i < V; i++){
+            for(auto y : l[i]){
+                indegree[y]++;
+            }
+        }
+        
+        //bfs
+        queue<int> q;
+        //1. step fing nodes with 0 in-degree
+        for(int i = 0; i < V; i++){
+            if(indegree[i] == 0)    
+                q.push(i);
+        }
+
+        //start removing elements form the queue
+        while (!q.empty()){
+            int node = q.front();
+            cout << node << " ";
+            q.pop();
+            //iterate over nbrs of that node and reduce their in-degree by 1
+            for(auto nbr : l[node]){
+                indegree[nbr]--;
+                if(indegree[nbr] == 0)
+                    q.push(nbr);
+            }
+        }
+    }
+};
+
+int main(){
+
+    Graph g(6);
+    g.addEdge(0, 2);
+    g.addEdge(1, 2);
+    g.addEdge(1, 4);
+    g.addEdge(2, 3);
+    g.addEdge(3, 5);
+    g.addEdge(2, 5);
+    g.addEdge(4, 5);
+
+    g.topological_sort();
+
+    return 0;
+}
