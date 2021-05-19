e842ca5c4b10eb92005ab833707eb7da66bc0464
froster02 <arushnaudiyal@gmail.com>
2021-05-19 19:40:35 +0530

    update

--- test.cc
+++ test.cc
@@ -1,44 +1,46 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
 #include <iostream>
+#include <algorithm>
 using namespace std;
-#define V 5
 
-bool colorGraph(int G[][V],int color[],int pos, int c){
-	
-	if(color[pos] != -1 && color[pos] !=c)
-		return false;
-
-	color[pos] = c;
-	bool ans = true;
-	for(int i=0;i<V;i++){
-		if(G[pos][i]){
-			if(color[i] == -1)
-				ans &= colorGraph(G,color,i,1-c);
-				
-			if(color[i] !=-1 && color[i] != 1-c)
-				return false;
-		}
-		if (!ans)
-			return false;
-	}
-	return true;
+void printArray(vector<int> arr, int n) {
+	for (int i = 0; i < n; i++)
+		cout << arr[i] << " ";
+	cout << endl;
 }
 
-bool isBipartite(int G[][V]){
-	int color[V];
-	for(int i=0;i<V;i++)
-		color[i] = -1;
-	int pos = 0;
-	return colorGraph(G,color,pos,1);
+void printSubsequences(vector<int> arr, int index, vector<int> subarr){
+
+	if (index == arr.size()) {
+		int l = subarr.size();
+		if (l != 0)
+			printArray(subarr, l);
+	} else {
+		printSubsequences(arr, index + 1, subarr);
+
+		subarr.push_back(arr[index]);
+
+		printSubsequences(arr, index + 1, subarr);
+	}
+	return;
 }
 
 int main() {
-	int G[][V] = {{0, 1, 1, 0, 1},
-				  {1, 0, 1, 1, 1},
-				  {1, 1, 0, 1, 0},
-				  {0, 1, 1, 0, 1},
-				  {0, 1, 0, 1, 0}};
-
-	isBipartite(G) ? cout<< "Yes Bipartite" : cout << "Not Bipartite";
-	return 0;
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    vector<int> b;
+    
+    for(int i = 0; i < n; i++){
+        int temp;
+        cin >> temp;
+        a.push_back(temp);
+    }
+    
+    printSubsequences(a, 0, b);
+    
+    return 0;
 }
 
