0e1c60acf22ef1b6e8f8c276a3ee335a6f6dbd50
froster02 <arushnaudiyal@gmail.com>
2021-07-01 12:57:32 +0530

    update

--- Graph_BFS.cc
+++ Graph_BFS.cc
@@ -39,11 +39,11 @@ template < typename T >
 
 int main() {
     Graph < int > g;
-    g.addEdge(0, 1);
     g.addEdge(1, 2);
-    g.addEdge(2, 3);
-    g.addEdge(3, 4);
-    g.addEdge(4, 5);
+    g.addEdge(1, 3);
+    g.addEdge(2, 4);
+    g.addEdge(1, 4);
+    g.addEdge(2, 5);
 
-    g.bfs(0);
+    g.bfs(1);
 }
--- Graph_BFSsingleSourceShortestPath.cc
+++ Graph_BFSsingleSourceShortestPath.cc
@@ -52,11 +52,11 @@ class graph{
 int main(){
     graph<int> g;
 
-    g.addEdge(0,1);
     g.addEdge(1,2);
-    g.addEdge(2,3);
-    g.addEdge(3,4);
-    g.addEdge(4,5);
+    g.addEdge(1,3);
+    g.addEdge(2,4);
+    g.addEdge(1,4);
+    g.addEdge(2,5);
 
-    g.bfs(0);
+    g.bfs(1);
 }
