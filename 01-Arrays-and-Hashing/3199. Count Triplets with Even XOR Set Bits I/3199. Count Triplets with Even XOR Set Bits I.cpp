962995d4d17142aede0d8880b7d5fcdd784ccb68
Arush Naudiyal <arushnaudiyal@gmail.com>
2026-07-17 07:42:50 +0530

    [3199] [Time Beats: 23.08%] [Memory Beats: 92.31%] - Leet2Hub

--- 01-Arrays-and-Hashing/3199. Count Triplets with Even XOR Set Bits I/3199. Count Triplets with Even XOR Set Bits I.cpp
+++ 01-Arrays-and-Hashing/3199. Count Triplets with Even XOR Set Bits I/3199. Count Triplets with Even XOR Set Bits I.cpp
@@ -1,19 +1,30 @@
 1class Solution {
 2public:
-3    int tripletCount(vector<int>& a, vector<int>& b, vector<int>& c) {
-4        int ans = 0;
+3    int cntBits(int x) {
+4        int cnt = 0;
 5
-6        for (int x : a) {
-7            for (int y : b) {
-8                for (int z : c) {
-9                    int xr = x ^ y ^ z;
+6        while (x > 0) {
+7            cnt = cnt + (x & 1); // check last bit
+8            x = x >> 1;
+9        }
 10
-11                    if (__builtin_popcount(xr) % 2 == 0)
-12                        ans++;
-13                }
-14            }
-15        }
+11        return cnt;
+12    }
+13
+14    int tripletCount(vector<int>& a, vector<int>& b, vector<int>& c) {
+15        int ans = 0;
 16
-17        return ans;
-18    }
-19};
+17        for (int x : a) {
+18            for (int y : b) {
+19                for (int z : c) {
+20                    int xr = x ^ y ^ z;
+21
+22                    if (cntBits(xr) % 2 == 0)
+23                        ans++;
+24                }
+25            }
+26        }
+27
+28        return ans;
+29    }
+30};
