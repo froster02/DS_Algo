5498722adfc621da8e22ee80d260e2cfe3df3b1b
Arush Naudiyal <arushnaudiyal@gmail.com>
2026-07-19 10:06:28 +0530

    [1570] [Time Beats: 41%] [Memory Beats: 100.00%] - Leet2Hub

--- 01-Arrays-and-Hashing/1570. Dot Product of Two Sparse Vectors/1570. Dot Product of Two Sparse Vectors.cpp
+++ 01-Arrays-and-Hashing/1570. Dot Product of Two Sparse Vectors/1570. Dot Product of Two Sparse Vectors.cpp
@@ -1,30 +1,37 @@
-class SparseVector {
-public:
-    vector<pair<int,int>>val;
-    SparseVector(vector<int> &nums) {
-        for(int i=0;i<nums.size();++i){
-            if(nums[i]!=0){
-                val.push_back({i,nums[i]});
-            }
-        }
-    }
-    
-    // Return the dotProduct of two sparse vectors
-    int dotProduct(SparseVector& vec) {
-        int i=0,j=0;
-        int Dot=0;
-        while(i<val.size() && j<vec.val.size()){
-            if(val[i].first== vec.val[j].first){
-                Dot += val[i].second * vec.val[j].second;
-                ++i;
-                ++j;
-            }
-            else if (val[i].first > vec.val[j].first){
-                j++;
-            }
-            else
-                i++;
-        }
-        return Dot;
-    }
-};
+1class SparseVector {
+2public:
+3    vector<pair<int, int>> v;
+4
+5    SparseVector(vector<int>& nums) {
+6
+7        int n = nums.size();
+8
+9        for (int i = 0; i < n; i++) {
+10            if (nums[i] != 0) {
+11                v.push_back({i, nums[i]});
+12            }
+13        }
+14    }
+15
+16    int dotProduct(SparseVector& vec) {
+17
+18        int ans = 0;
+19
+20        int i = 0, j = 0;
+21
+22        while (i < v.size() && j < vec.v.size()) {
+23
+24            if (v[i].first == vec.v[j].first) {
+25                ans += v[i].second * vec.v[j].second;
+26                i++;
+27                j++;
+28            } else if (v[i].first < vec.v[j].first) {
+29                i++;
+30            } else {
+31                j++;
+32            }
+33        }
+34
+35        return ans;
+36    }
+37};
