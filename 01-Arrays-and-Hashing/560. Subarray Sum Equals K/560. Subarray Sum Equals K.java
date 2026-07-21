17e4f07e673fb2cf46d724828ad2921cd87b61f0
Arush Naudiyal <arushnaudiyal@gmail.com>
2026-07-21 22:05:26 +0530

    [560] [Time Beats: 13.51%] [Memory Beats: 79.10%] - Leet2Hub

--- 01-Arrays-and-Hashing/560. Subarray Sum Equals K/560. Subarray Sum Equals K.java
+++ 01-Arrays-and-Hashing/560. Subarray Sum Equals K/560. Subarray Sum Equals K.java
@@ -1,1 +1,17 @@
-class Solution {    public int subarraySum(int[] nums, int k) {        int n = nums.length;        int count = 0;        for(int i = 0; i < n; i++){            for(int j = i; j < n; j++){        }                int sum = 0;            }                for(int x = i; x <= j; x++){                    sum = sum + nums[x];                }    }                if(sum == k)    count++;        return count;}
+1class Solution {
+2    public int subarraySum(int[] nums, int k) {
+3        int n = nums.length;
+4        int count = 0;
+5
+6        for(int i = 0; i < n; i++){
+7            int sum = 0;
+8            for(int j = i; j < n; j++){
+9                sum += nums[j];
+10                if(sum == k)    
+11                    count++;
+12            }
+13        }
+14
+15        return count;
+16    }
+17}
