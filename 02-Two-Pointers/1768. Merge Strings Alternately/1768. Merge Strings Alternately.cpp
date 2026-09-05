404effbbe6f5d6113389796f8766cf0669ef0e07
Arush Naudiyal <arushnaudiyal@gmail.com>
2026-09-05 08:52:12 +0530

    [1768] [Time Beats: 100.00%] [Memory Beats: 29.90%] - Leet2Hub

--- 02-Two-Pointers/1768. Merge Strings Alternately/1768. Merge Strings Alternately.cpp
+++ 02-Two-Pointers/1768. Merge Strings Alternately/1768. Merge Strings Alternately.cpp
@@ -1,13 +1,13 @@
 1class Solution {
 2public:
 3    string mergeAlternately(string word1, string word2) {
-4        int m = word1.size(), n = word2.size();
-5        string ans = ;
-6
-7        for (int i = 0; i < max(m, n); i++) {
-8            if (i < m)
+4        int n1 = word1.size();
+5        int n2 = word2.size();
+6        string ans;
+7        for (int i = 0; i < max(n1, n2); i++) {
+8            if (i < n1)
 9                ans += word1[i];
-10            if (i < n)
+10            if (i < n2)
 11                ans += word2[i];
 12        }
 13
