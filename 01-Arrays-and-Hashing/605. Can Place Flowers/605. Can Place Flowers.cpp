d8c74148aadb470219b08459a1ada20e25e00398
Arush Naudiyal <arushnaudiyal@gmail.com>
2026-09-05 15:39:04 +0530

    [605] [Time Beats: 10.83%] [Memory Beats: 43.78%] - Leet2Hub

--- 01-Arrays-and-Hashing/605. Can Place Flowers/605. Can Place Flowers.cpp
+++ 01-Arrays-and-Hashing/605. Can Place Flowers/605. Can Place Flowers.cpp
@@ -1,19 +1,28 @@
 1class Solution {
 2public:
 3    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-4        int c = 0, size = flowerbed.size(); 
-5        
-6        for (int i = 0; i < size; i++) {
-7            if (flowerbed[i] == 0 &&
-8                (i == 0 || flowerbed[i - 1] == 0) &&         // check if not the first element or the previous plot of the current plot is empty or not
-9                (i == size - 1 || flowerbed[i + 1] == 0)) {  // check if the idx is not the last idx and next of the current plot is empty or not 
-10                
-11                flowerbed[i] = 1;  
-12                c++;               
-13                i++;              
-14            }
-15        }
-16
-17        return c >= n;
-18    }
-19};
+4        int len = flowerbed.size();
+5
+6        for (int i = 0; i < len; i++) {
+7
+8            if (flowerbed[i] == 0) {
+9
+10                // (i == 0)  -> if i'm at the first idx.
+11                // (len - 1) -> if i'm at the last idx.
+12                bool leftKhali  = ((i == 0)         || (flowerbed[i - 1] == 0));
+13                bool rightKhali = ((i == (len - 1)) || (flowerbed[i + 1] == 0));
+14
+15                if (leftKhali && rightKhali) {
+16                    flowerbed[i] = 1;
+17                    n--;
+18
+19                    if (n == 0)
+20                        return true;
+21                }
+22            }
+23        }
+24
+25        return (n <= 0);  
+26    }
+27};
+28
