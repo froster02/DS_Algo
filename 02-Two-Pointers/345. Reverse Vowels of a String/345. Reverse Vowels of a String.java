497c5ae4e6239875ebea9955ce6181e6e7db5131
Arush Naudiyal <arushnaudiyal@gmail.com>
2026-09-06 09:47:48 +0530

    [345] [Time Beats: 99.26%] [Memory Beats: 49.12%] - Leet2Hub

--- 02-Two-Pointers/345. Reverse Vowels of a String/345. Reverse Vowels of a String.java
+++ 02-Two-Pointers/345. Reverse Vowels of a String/345. Reverse Vowels of a String.java
@@ -1,1 +1,33 @@
-class Solution {    private boolean vowel(char ch) {        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch ==         'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U';    }    public String reverseVowels(String s) {        if (s == null || s.length() < 2)            return s;        char[] chars = s.toCharArray();        int n = chars.length;        int i = 0;        int j = n - 1;        while (i < j) {            if (vowel(chars[i]) && vowel(chars[j])) {                char temp = chars[i];                chars[i] = chars[j];                chars[j] = temp;                i++;                j--;            } else if (!vowel(chars[i])) {                i++;            } else {
+1class Solution {
+2
+3    private boolean vowel(char ch) {
+4        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U';
+5    }
+6
+7    public String reverseVowels(String s) {
+8        if (s == null || s.length() < 2)
+9            return s;
+10
+11        char[] chars = s.toCharArray();
+12        int n = chars.length;
+13
+14        int i = 0;
+15        int j = n - 1;
+16
+17        while (i < j) {
+18            if (vowel(chars[i]) && vowel(chars[j])) {
+19                char temp = chars[i];
+20                chars[i] = chars[j];
+21                chars[j] = temp;
+22                i++;
+23                j--;
+24            } else if (!vowel(chars[i])) {
+25                i++;
+26            } else {
+27                j--;
+28            }
+29        }
+30
+31        return new String(chars);
+32    }
+33}
