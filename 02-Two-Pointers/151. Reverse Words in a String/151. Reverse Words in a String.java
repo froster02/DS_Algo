440c0672df5b7b9ca6dbfabfdfb86dee2ea3eeb1
Arush Naudiyal <arushnaudiyal@gmail.com>
2026-09-07 22:57:17 +0530

    [151] [Time Beats: 5.05%] [Memory Beats: 91.65%] - Leet2Hub

--- 02-Two-Pointers/151. Reverse Words in a String/151. Reverse Words in a String.java
+++ 02-Two-Pointers/151. Reverse Words in a String/151. Reverse Words in a String.java
@@ -1,1 +1,25 @@
-class Solution {    public String reverseWords(String s) {        String[] words = s.trim().split(\\s+); // to handles multiple spaces        StringBuilder sb = new StringBuilder();        for (int i = words.length - 1; i >= 0; i--) {            sb.append(words[i]);            if (i > 0)                sb.append(' ');        }        return sb.toString();    }}
+1class Solution {
+2    public String reverseWords(String s) {
+3        int n = s.length();
+4        StringBuilder sb = new StringBuilder();
+5
+6        int i = n - 1; // start from the end
+7
+8        while (i >= 0) {
+9            while (i >= 0 && s.charAt(i) == ' ')
+10                i--; // skip trailing spaces
+11
+12            if (i < 0)
+13                break;
+14
+15            int end = i;
+16            while (i >= 0 && s.charAt(i) != ' ')
+17                i--; // find word start
+18
+19            sb.append(s, i + 1, end + 1); // append the word
+20            sb.append(' ');
+21        }
+22
+23        return sb.toString().trim();
+24    }
+25}
