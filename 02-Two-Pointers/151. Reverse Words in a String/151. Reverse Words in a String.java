228646325e950d3bb3ac4427bbd4b533f1f0810e
Arush Naudiyal <arushnaudiyal@gmail.com>
2026-09-07 21:50:44 +0530

    [151] [Time Beats: N/A] [Memory Beats: N/A] - Leet2Hub

---
+++ 02-Two-Pointers/151. Reverse Words in a String/151. Reverse Words in a String.java
@@ -0,0 +1,1 @@
+class Solution {    public String reverseWords(String s) {        String[] words = s.trim().split(\\s+); // to handles multiple spaces        StringBuilder sb = new StringBuilder();        for (int i = words.length - 1; i >= 0; i--) {            sb.append(words[i]);            if (i > 0)                sb.append(' ');        }        return sb.toString();    }}
