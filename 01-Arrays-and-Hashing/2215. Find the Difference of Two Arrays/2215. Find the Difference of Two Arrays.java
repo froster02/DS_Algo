4f001d45d8874916cbcb32d53fbaeae6c7f729da
Arush Naudiyal <arushnaudiyal@gmail.com>
2026-09-24 06:21:53 +0530

    [2215] [Time Beats: N/A] [Memory Beats: N/A] - Leet2Hub

--- 01-Arrays-and-Hashing/2215. Find the Difference of Two Arrays/2215. Find the Difference of Two Arrays.java
+++ 01-Arrays-and-Hashing/2215. Find the Difference of Two Arrays/2215. Find the Difference of Two Arrays.java
@@ -1,26 +1,33 @@
 1class Solution {
 2    public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
-3        Set<Integer> m1 = new HashSet<>();
-4        Set<Integer> m2 = new HashSet<>();
+3        Set<Integer> st1 = new HashSet<>();
+4        Set<Integer> st2 = new HashSet<>();
 5
-6        for (int num : nums1)
-7            m1.add(num);
-8        for (int num : nums2)
-9            m2.add(num);
+6        // fill nums1 values to set
+7        for (int itr : nums1) {
+8            st1.add(itr);
+9        }
 10
-11        List<Integer> diff1 = new ArrayList<>();
-12        List<Integer> diff2 = new ArrayList<>();
-13
-14        for (int itr : m1) {
-15            if (!m2.contains(itr))
-16                diff1.add(itr);
-17        }
+11        // fill nums2 values to set
+12        for (int itr : nums2) {
+13            st2.add(itr);
+14        }
+15
+16        List<Integer> diff1 = new ArrayList<>();
+17        List<Integer> diff2 = new ArrayList<>();
 18
-19        for (int itr : m2) {
-20            if (!m1.contains(itr))
-21                diff2.add(itr);
-22        }
-23
-24        return List.of(diff1, diff2);
-25    }
-26}
+19        for (int itr : st1) {
+20            if (!st2.contains(itr)) {
+21                diff1.add(itr);
+22            }
+23        }
+24
+25        for (int itr : st2) {
+26            if (!st1.contains(itr)) {
+27                diff2.add(itr);
+28            }
+29        }
+30
+31        return List.of(diff1, diff2);
+32    }
+33}
